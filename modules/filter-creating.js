5a4e878847991999ddc5d084a41471b0b9bfcede
dsymonov <dsymonov@justcoded.co>
2020-08-25 17:43:39 +0300

    add private methods to class

--- modules/filter-creating.js
+++ modules/filter-creating.js
@@ -1,7 +1,8 @@
 class Filter {
-  constructor(params) {
-    this.filterAttr = params.filterAttr;
-    this.formAttr = params.formAttr;
+  constructor(options) {
+    this.filterAttr = options.filterAttr;
+    this.formAttr = options.formAttr;
+    this.$form = document.querySelector(`[${this.formAttr}]`);
     this.url = new URL(window.location.href);
     this.filtersUrl = new URLSearchParams(decodeURIComponent(this.url.searchParams));
   }
@@ -12,16 +13,16 @@ class Filter {
   }
 
   static _checkInputType(input) {
-    this.tagName = input.tagName.toLowerCase();
-    this.inputType = null;
+    const tagName = input.tagName.toLowerCase();
+    const inputType = null;
 
-    if (this.tagName === 'select') {
-      this.inputType = input.multiple ? 'select-multi' : 'select-single';
-    } else if (this.tagName === 'input') {
-      this.inputType = input.type === 'radio' ? 'radio' : 'checkbox';
+    if (tagName === 'select') {
+      inputType = input.multiple ? 'select-multi' : 'select-single';
+    } else if (tagName === 'input') {
+      inputType = input.type === 'radio' ? 'radio' : 'checkbox';
     }
 
-    return this.inputType;
+    return inputType;
   }
 
   static _parseFiltersFromUrl(paramsUrl) {
@@ -37,5 +38,80 @@ class Filter {
     });
 
     return objectFilters;
+  }
+
+  static _updateInputsFromUrl(paramsUrl) {
+    objectFilters = Filter._parseFiltersFromUrl(paramsUrl);
+
+    if (objectFilters.filters.length === 0) return;
+
+    Object.keys(objectFilters.filters).forEach((type) => {
+      const filter = document.querySelector(`[${this.filterAttr}="${type}"]`);
+      const filterType = Filter._checkInputType(filter);
+
+      if (filterType === 'select-single') {
+        document.querySelector(`[${this.filterAttr}="${type}"]`).value = objectFilters.filters[type][0];
+      } else if (filterType === 'radio') {
+        document.querySelector(`[${this.filterAttr}="${type}"][value="${objectFilters.filters[type][0]}"]`).checked = true;
+      } else if (filterType === 'checkbox') {
+        objectFilters.filters[type].forEach((val) => {
+          document.querySelector(`[${this.filterAttr}="${type}"][value="${val}"]`).checked = true;
+        });
+      }
+    });
+  }
+
+  static _updateUrlFromInputs(e) {
+    const { target } = e;
+    const filterName = target.getAttribute(`${this.filterAttr}`);
+    const filterValue = target.value;
+    const filterType = Filter._checkInputType(target);
+    const isFilterChecked = target.checked;
+    const filterParams = `${filtersUrl.get(filterName)}`.split(' ');
+
+    if ((filterType === 'select-single') || filterType === 'radio') {
+      if (filtersUrl.has(filterName)) {
+        if (filterValue !== '') {
+          filtersUrl.set(filterName, filterValue);
+        } else {
+          filtersUrl.delete(filterName);
+        }
+      } else {
+        filtersUrl.append(filterName, filterValue);
+      }
+    } else if (filterType === 'checkbox') {
+      if (filtersUrl.has(filterName)) {
+        if (isFilterChecked) {
+          filtersUrl.set(filterName, `${filtersUrl.get(filterName)} ${filterValue}`);
+        } else if (filterParams.length > 1) {
+          filtersUrl.delete(filterName);
+          filterParams.splice(filterParams.indexOf(filterValue), 1);
+          filtersUrl.set(filterName, filterParams.join(' '));
+        } else {
+          filtersUrl.delete(filterName);
+        }
+      } else {
+        filtersUrl.append(filterName, filterValue);
+      }
+    }
+  }
+
+  static _setFiltersToUrl(newUrl, setFiltersUrl) {
+    const filterUrl = new URL(newUrl);
+
+    filterUrl.search = setFiltersUrl;
+    window.history.pushState(null, null, filterUrl);
+
+    Filter._updateUrl();
+  }
+
+  static _resetUrl() {
+    const emptyFiltersUrl = new URLSearchParams('');
+
+    Filter._setFiltersToUrl(this.url, emptyFiltersUrl);
+  }
+
+  static _resetInputs() {
+    this.$form.reset();
   }
 }
