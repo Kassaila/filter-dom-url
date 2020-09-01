10bf4eaa20700fc208d2401c5d81814d21540a84
dsymonov <dsymonov@justcoded.co>
2020-09-01 14:04:17 +0300

    add minify version of file

--- tasks/build-js.js
+++ tasks/build-js.js
@@ -17,69 +17,10 @@ module.exports = function () {
       .on('error', (error) => notifier.error(error.message, 'Main JS compiling error', done))
       .pipe(rename(`${global.file.mainJs}.js`))
       .pipe(gulp.dest(`./${global.folder.build}`))
+      .pipe(babel({
+        presets: ['minify'],
+      }))
       .pipe(rename(`${global.file.mainJsMin}.js`))
       .pipe(gulp.dest(`./${global.folder.build}`));
   };
 };
-
-// const webpack = require('webpack');
-// const path = require('path');
-
-// const notifier = require('../helpers/notifier');
-// const global = require('../gulp-config.js');
-
-// module.exports = function () {
-//   const production = global.isProduction();
-
-//   return (done) => {
-//     try {
-//       const config = {
-//         mode: 'none',
-//         entry: {
-//           [global.file.mainJs]: `./${global.folder.src}/index.js`,
-//         },
-//         output: {
-//           path: path.resolve(global.folder.build, ''),
-//           filename: `[name].js`,
-//         },
-//         optimization: {
-//           minimize: false,
-//         },
-//         module: {
-//           rules: [
-//             {
-//               test: /\.m?js$/,
-//               exclude: /(node_modules)/,
-//               use: {
-//                 loader: 'babel-loader',
-//               }
-//             }
-//           ]
-//         },
-//       };
-
-//       webpack(config, (error, stats) => {
-//         if (production) {
-//           console.log(
-//             stats.toString({
-//               version: false,
-//               hash: false,
-//               chunks: false,
-//               colors: true,
-//             })
-//           );
-//         }
-
-//         if (stats) {
-//           return done();
-//         }
-
-//         if (error) {
-//           throw new Error(error);
-//         }
-//       });
-//     } catch (error) {
-//       notifier.error(error, 'JS compiling error', done);
-//     }
-//   };
-// };
